a6880c508c5894c1c8b5ee6f40493df5f7daeef0
Alex Baykov <baykov-1988@mail.ru>
2024-11-22 21:55:57 +0300

    modified:   components/Button/Button.module.css 	modified:   components/Button/Button.tsx 	modified:   pages/index.tsx 	modified:   styles/globals.css

--- top-app/components/Button/Button.tsx
+++ top-app/components/Button/Button.tsx
@@ -3,12 +3,12 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames'
 
-export const Button = ({appearence, children}: ButtonProps): JSX.Element => {
-			return 
-			(<button className={cn(styles.button,
-			{[styles.primary]: appearence == 'primary',
+export const Button = ({appearence, children}: ButtonProps): JSX.Element => { 
+			return (<button className={cn(styles.button,
+		{
+			[styles.primary]: appearence == 'primary',
 			[styles.primary]: appearence == 'ghost',
-			})}>
-				{children}
-			</button>);
+		})}>
+		{children}
+	</button>);
 	}
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home(): JSX.Element {
       <>
       <Htag tag='h1'>Текст</Htag>
       <Button appearence='primary'>Кнопка</Button>
+      <Button appearence='ghost'>Кнопка</Button>
       </>
     
   );
