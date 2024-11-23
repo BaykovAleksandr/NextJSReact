802fd76f627832c8c9351db41060b03c96b2bf14
Aleksandr <baykov-1988@mail.ru>
2024-11-23 17:35:10 +0300

    top-app/next.config.ts 	new file:   top-app/next.env.d.custom 	new file:   top-app/svg.d.ts 	modified:   top-app/tsconfig.json

--- top-app/next.config.ts
+++ top-app/next.config.ts
@@ -9,13 +9,23 @@
 
  /** @type {import('next').NextConfig} */
   module.exports = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/i,
-          issuer: {
-            test: /\.(js|ts)x?$/,
-        },
-      use: ['@svgr/webpack'],
-      })
-    }
-  }
+	images: {
+		domains: ['courses-top.ru']
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{ removeViewBox: false }],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
+
+		return config;
+	},
+};
