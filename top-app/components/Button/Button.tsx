6d53bd6168cb2465abc97dbf4d485ed3038c39b1
Aleksandr <baykov-1988@mail.ru>
2024-11-23 12:03:08 +0300

    modified:   top-app/components/Button/Button.module.css 	modified:   top-app/components/Button/Button.props.ts 	modified:   top-app/components/Button/Button.tsx 	modified:   top-app/components/Button/Button.module.css 	modified:   top-app/components/Button/Button.props.ts 	modified:   top-app/components/Button/Button.tsx 	modified:   top-app/next.config.ts 	modified:   top-app/pages/index.tsx 	top-app/components/Button/arrow.svg

--- top-app/components/Button/Button.tsx
+++ top-app/components/Button/Button.tsx
@@ -3,12 +3,15 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames'
 
-export const Button = ({appearence, children}: ButtonProps): JSX.Element => { 
-			return (<button className={cn(styles.button,
+export const Button = ({appearence, children, className, ...props}: ButtonProps): JSX.Element => { 
+			return (
+			<button
+				className={cn(styles.button, className,
 		{
 			[styles.primary]: appearence == 'primary',
 			[styles.primary]: appearence == 'ghost',
-		})}>
+		})}
+		{...props}>
 		{children}
 	</button>);
 	}
